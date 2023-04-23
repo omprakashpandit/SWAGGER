b0f78c029deac1b2f9259c20704a88a99994a2ae
omprakashpandit <32616762+omprakashpandit@users.noreply.github.com>
2023-04-23 08:07:37 +0530

    Add files via upload

---
+++ springboot-swagger-3/src/main/java/com/pak/SwaggerSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package com.pak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringDemoApplication.class, args);
+	}
+
+}
+///http://localhost:8080/swagger-ui.html
